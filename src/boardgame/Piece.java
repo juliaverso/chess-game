3c714b0121d6ba98cadea5f93d500c51af5fe8ba
Júlia Lopes <juliacassia.lopes@gmail.com>
2024-01-23 18:52:42 -0300

    Printing possible moves

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -19,15 +19,15 @@ public abstract class Piece {
 	}
 	
 	public boolean isThereAnyPossibleMove() {
-		boolean [][] mat = possibleMoves();
-		for(int i=0; i<mat.length; i++) {
-			for( int j=0; j<mat.length; j++) {
-				if(mat[i][j]) {
-					return true; 
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
 				}
 			}
 		}
-		return false; 
+		return false;
 	}
 	
 
